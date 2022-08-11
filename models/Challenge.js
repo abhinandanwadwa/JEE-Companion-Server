7e8b1bd279065fab464f409950c56e35a7eec21d
Abhi <abhinandanwadhwa8@gmail.com>
2022-08-11 13:38:41 +0530

    Updating Challenge Schema by adding sender and receiver scores

--- models/Challenge.js
+++ models/Challenge.js
@@ -50,6 +50,14 @@ const ChallengeSchema = new Schema({
         type: Boolean,
         default: false
     },
+    ReceiverScore: {
+        type: Number,
+        default: null
+    },
+    SenderScore: {
+        type: Number,
+        default: null
+    }
 }, {timestamps: true});
 
 module.exports = mongoose.model('challenge', ChallengeSchema);
